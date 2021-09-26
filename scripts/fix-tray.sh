c05db6d87922536c2369f721b285343330a1dbb1
Tao Wang <twang2218@gmail.com>
2021-09-27 09:03:10 +1000

    Refine the AppArmor profiles
    Signed-off-by: Tao Wang <twang2218@gmail.com>

--- scripts/fix-tray.sh
+++ scripts/fix-tray.sh
@@ -6,14 +6,14 @@ SNAP_APPARMOR_PROFILE=/var/lib/snapd/apparmor/profiles/snap.${SNAP_NAME}.${SNAP_
 echo "Fix AppArmor profile of ${SNAP_NAME}..."
 cp ${SNAP_APPARMOR_PROFILE} ${SNAP_APPARMOR_PROFILE}.bak
 patch ${SNAP_APPARMOR_PROFILE} <<EOF
---- snap.qqmusic.qqmusic.bak	2021-09-19 12:47:04.518276613 +1000
-+++ snap.qqmusic.qqmusic	2021-09-19 12:44:02.897018611 +1000
-@@ -973,21 +973,21 @@
+--- /var/lib/snapd/apparmor/profiles/snap.qqmusic-snap.qqmusic-snap.orig	2021-09-27 08:45:50.374841541 +1000
++++ /var/lib/snapd/apparmor/profiles/snap.qqmusic-snap.qqmusic-snap	2021-09-27 08:47:29.326838199 +1000
+@@ -1041,21 +1041,21 @@
  # dbusmenu
  dbus (send)
      bus=session
 -    path=/{MenuBar{,/[0-9A-F]*},com/canonical/menu/[0-9A-F]*}
-+    path=/{MenuBar{,/[0-9A-F]*},com/canonical/menu/[0-9A-F]*,com/canonical/dbusmenu}
++    path=/{MenuBar{,/[0-9A-F]*},com/canonical/{menu/[0-9A-F]*,dbusmenu}}
      interface=com.canonical.dbusmenu
      member="{LayoutUpdated,ItemsPropertiesUpdated}"
      peer=(name=org.freedesktop.DBus, label=unconfined),
@@ -21,7 +21,7 @@ patch ${SNAP_APPARMOR_PROFILE} <<EOF
  dbus (receive)
      bus=session
 -    path=/{MenuBar{,/[0-9A-F]*},com/canonical/menu/[0-9A-F]*}
-+    path=/{MenuBar{,/[0-9A-F]*},com/canonical/menu/[0-9A-F]*,com/canonical/dbusmenu}
++    path=/{MenuBar{,/[0-9A-F]*},com/canonical/{menu/[0-9A-F]*,dbusmenu}}
      interface="{com.canonical.dbusmenu,org.freedesktop.DBus.Properties}"
      member=Get*
      peer=(label=unconfined),
@@ -29,13 +29,13 @@ patch ${SNAP_APPARMOR_PROFILE} <<EOF
  dbus (receive)
      bus=session
 -    path=/{MenuBar{,/[0-9A-F]*},com/canonical/menu/[0-9A-F]*}
-+    path=/{MenuBar{,/[0-9A-F]*},com/canonical/menu/[0-9A-F]*,com/canonical/dbusmenu}
++    path=/{MenuBar{,/[0-9A-F]*},com/canonical/{menu/[0-9A-F]*,dbusmenu}}
      interface=com.canonical.dbusmenu
      member="{AboutTo*,Event*}"
      peer=(label=unconfined),
 EOF
 
-diff -u ${SNAP_APPARMOR_PROFILE}.bak ${SNAP_APPARMOR_PROFILE}
+diff -u ${SNAP_APPARMOR_PROFILE}.orig ${SNAP_APPARMOR_PROFILE}
 
 echo "Reloading the AppArmor profile for ${SNAP_NAME}..."
 apparmor_parser -r ${SNAP_APPARMOR_PROFILE}
